f39cb4263bc5139f007db8e885c00cb6c144d0ab
Jussi Viinikka <jussi.viinikka@gmail.com>
2021-01-07 13:35:02 +0200

    Add function to clean build artifacts
    command: python setup.py clean --all

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import os
 import pathlib
 import setuptools
 from distutils.core import setup, Extension
+from distutils.command.clean import clean as Clean
 import distutils.util
+import shutil
 from Cython.Build import cythonize
 import numpy
+
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -68,6 +71,39 @@ try:
     numpy_include = numpy.get_include()
 except AttributeError:
     numpy_include = numpy.get_numpy_include()
+
+
+# Copied from scikit-learn
+class CleanCommand(Clean):
+    description = "Remove build artifacts from the source tree"
+
+    def run(self):
+        Clean.run(self)
+        # Remove c files if we are not within a sdist package
+        cwd = os.path.abspath(os.path.dirname(__file__))
+        remove_c_files = not os.path.exists(os.path.join(cwd, 'PKG-INFO'))
+        if remove_c_files:
+            print('Will remove generated .c files')
+        if os.path.exists('build'):
+            shutil.rmtree('build')
+        for dirpath, dirnames, filenames in os.walk('sumu'):
+            for filename in filenames:
+                if any(filename.endswith(suffix) for suffix in
+                       (".so", ".pyd", ".dll", ".pyc")):
+                    os.unlink(os.path.join(dirpath, filename))
+                    continue
+                extension = os.path.splitext(filename)[1]
+                if remove_c_files and extension in ['.c', '.cpp']:
+                    pyx_file = str.replace(filename, extension, '.pyx')
+                    if os.path.exists(os.path.join(dirpath, pyx_file)):
+                        os.unlink(os.path.join(dirpath, filename))
+            for dirname in dirnames:
+                if dirname == '__pycache__':
+                    shutil.rmtree(os.path.join(dirpath, dirname))
+
+
+cmdclass = {'clean': CleanCommand}
+
 
 exts = [
 
@@ -167,5 +203,6 @@ setup(
         "numpy",
         "scipy"
     ],
+    cmdclass=cmdclass,
     ext_modules=cythonize(exts, language_level="3", compiler_directives=COMPILER_DIRECTIVES)
 )
